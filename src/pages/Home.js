468148cf5147e47dccdf98ad1b814da381ac1b08
mariorojasac <77466868+mariorojasac@users.noreply.github.com>
2021-10-29 17:58:06 -0400

    adding about sections

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,30 @@
 const Home = (props) => {
-    return (
-      <>
-        <div>Mason's App</div>
-        <button>Login</button>
-        <button>sign Up</button>
-      </>
-    );
+  return (
+    <>
+      <div>Mason's App</div>
+      <button>Login</button>
+          <button>sign Up</button>
+          <div><p>scroll down</p></div>
+<hr />
+      <div>
+        <p>About</p>
+        <p>Our Mission</p>
+        <p>Contact</p>
+      </div>
+
+      <div className="container">
+        <div className="card">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam
+            doloremque voluptatibus nihil earum magnam ullam quod, facilis
+            accusamus consequatur unde corrupti repudiandae perspiciatis totam
+            hic voluptatem fugiat veritatis, veniam eligendi?
+          </p>
+        </div>
+          </div>
+          <hr />
+    </>
+  );
 };
 
 export default Home;
